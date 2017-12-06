f8aacf6489bd3d64700eed98308b38ce18eaf687
Bonny Sveta <bonnysveta@gmail.com>
2017-12-06 23:44:45 +1000

    hamburger fixed but not rotate

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,6 +2,7 @@ var menus = document.getElementsByClassName('hamburger-menu');
 
 [].forEach.call(menus, function(m) {
     m.addEventListener('click', function() {
+        console.log('open');
         m.classList.toggle('open');
     });
 });
