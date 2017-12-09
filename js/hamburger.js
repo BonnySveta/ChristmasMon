b159d29008c8ab86ee6dad1d0758bb68c43604e5
user-PC\user <repinsky>
2017-12-09 22:16:59 +1000

    modal menu anchors logic

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,7 +2,6 @@ var menus = document.getElementsByClassName('hamburger-menu');
 
 [].forEach.call(menus, function(m) {
     m.addEventListener('click', function() {
-        console.log('open');
         m.classList.toggle('open');
     });
 });
